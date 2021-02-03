2eb41d34abe7d5e14475f17615ac4ab503a165eb
Gene <gorange310@gmail.com>
2021-02-03 20:15:54 +0800

    route: root, login. gem 'rails-controller-testing'

--- spec/requests/chatroom_request_spec.rb
+++ spec/requests/chatroom_request_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "Chatrooms", type: :request do
-
+  it "route root path to chatroom#index" do
+    get "/"
+    expect(response).to render_template :index
+  end
 end
--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
+  it "route login to session#new" do
+    get "/login"
+    expect(response).to render_template :new
+  end
 end
