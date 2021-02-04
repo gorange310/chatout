de349719b955915072de416f83285f4511ce514e
Gene <gorange310@gmail.com>
2021-02-04 20:33:44 +0800

    add _message partial and refactor code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :messages
+  has_many :messages, dependent: :destroy
   validates :username, presence: true,
     uniqueness: { case_sensitive: false },
     length: { minimum: 3, maximum: 30 }
