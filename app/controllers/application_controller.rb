3d30db965dfb02d5469ed8774a574716ecc8b38b
Gene <gorange310@gmail.com>
2021-02-08 23:55:47 +0800

    add authenticate: create and destroy sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  helper_method :current_user, :logged_in?
+  
+  def current_user
+    @current ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:alert] = "You must be logged in to perform that action"
+      redirect_to login_path
+    end
+  end
+
+  def not_found
+    render file: "public/404.html", layout: false, status: 404
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,22 @@
 class SessionsController < ApplicationController
   def new
-    
+  end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash.now[:success] = "Log in successfully"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Something wrong"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Log out successfully"
+    redirect_to root_path
   end
 end
