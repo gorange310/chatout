1e0fdf155c2846d35a61c4ae73982472c40ea448
Gene <gorange310@gmail.com>
2021-02-09 19:43:56 +0800

    Modify and broadcast messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,20 @@
 class MessagesController < ApplicationController
   def create
-    @message = Message.new(message_params)
-    if @message.save
-      flash[:success] = "Typed in"
+    message = current_user.messages.build(message_params)
+    if message.save
+      # flash[:success] = "Typed in"
+      ActionCable.server.broadcast "chatroom_channel", 
+                                  modified_message: message_render(message)
     end
     redirect_to root_path
-
   end
 
   private
   def message_params
     params.require(:message).permit(:text)
+  end
+
+  def message_render(message)
+    render(partial: "message", locals: {message: message} )
   end
 end
